fa7cd2801c51b6078ec1f1934b7abfc22bc1c1ba
Alastair Revell <ar15153@outlook.com>
2019-02-11 15:18:01 +0000

    Step 7: Add dedcut and top-up functions

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,21 @@
 class Oystercard
 
   attr_reader :balance
+  LIMIT = 90
 
   def initialize
-    @balance = 10
+    @balance = 0
   end
+
+  def top_up(value)
+    fail "Balance limit is £90" unless @balance + value <= LIMIT
+    @balance += value
+  end
+
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,22 @@ require "oystercard"
 describe Oystercard do
 
   it 'has a balance of zero' do
-    expect(subject.balance).to eq 0 
+    expect(subject.balance).to eq 0
+  end
+
+  it 'can be topped up' do
+    subject.top_up(10)
+    expect(subject.balance).to eq(10)
+  end
+
+  it 'will not accept more than £90' do
+    expect{subject.top_up(91)}.to raise_error "Balance limit is £90"
+  end
+
+  it 'deducts the fare from the balance' do
+    subject.top_up(10)
+    subject.deduct(5)
+    expect(subject.balance).to eq(5)
   end
 
 end
